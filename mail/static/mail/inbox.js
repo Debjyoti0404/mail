14bec119c34128bafce553122ea0792d3d110da5
Debjyoti0404 <debsen2003@gmail.com>
2023-08-30 18:18:04 +0530

    implemented inbox, sent view
    Signed-off-by: Debjyoti0404 <debsen2003@gmail.com>

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -35,15 +35,16 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  mail_preview(mailbox);
 }
 
 function send_mail() {
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
-        recipients: 'sankha@gmail.com',
-        subject: 'Meeting time',
-        body: 'How about we meet tomorrow at 3pm?'
+        recipients: document.getElementById('compose-recipients').value,
+        subject: document.getElementById('compose-subject').value,
+        body: document.getElementById('compose-body').value
     })
   })
   .then(response => response.json())
@@ -51,4 +52,39 @@ function send_mail() {
     console.log(result)
   })
   .then(load_mailbox('sent'));
+}
+
+function mail_preview(mailbox) {
+  let path = '/emails/';
+  path = path.concat(mailbox);
+  fetch(path)
+  .then(response => response.json())
+  .then(result => {
+    result.forEach(element => {
+      const element_div = document.createElement('div');
+      const sender = document.createElement('p');
+      sender.style = `
+        font-weight: bold;
+        margin-right: 10px;
+      `;
+      const subject = document.createElement('p');
+      const time = document.createElement('p');
+      time.style = `
+        font-weight: 100;
+        margin-left: auto;
+      `;
+      sender.innerHTML = element.sender;
+      subject.innerHTML = element.subject;
+      time.innerHTML = element.timestamp;
+      element_div.appendChild(sender);
+      element_div.appendChild(subject);
+      element_div.appendChild(time);
+      element_div.style = `
+        border: solid black 1px;
+        display: flex;
+      `
+      element_div.addEventListener('click', () => {console.log(element)});
+      document.getElementById('emails-view').append(element_div);
+    });
+  })
 }
